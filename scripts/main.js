7adf37bdc588b5d935e0aa9d3c58da2424cb8af0
KamalAMB <kamal.alimbae@gmail.com>
2025-07-01 09:08:40 +0200

    ajout script Connexion manquant

--- scripts/main.js
+++ scripts/main.js
@@ -16,3 +16,12 @@ window.addEventListener("click", (e) => {
     modal.classList.add("hidden");
   }
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.nav');
+
+  burger.addEventListener('click', () => {
+    nav.classList.toggle('active');
+  });
+});
